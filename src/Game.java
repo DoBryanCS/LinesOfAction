4e98082464bbb39ed8d45bfe0ae26644d7bbf237
Bryan <do.bryan.cs@gmail.com>
2025-11-02 17:44:08 -0500

    Initialize board

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,15 @@
 public class Game {
+    private int[][] board = new int[8][8];
 
+    public Game(int[][] initialBoard) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                this.board[i][j] = initialBoard[i][j];
+            }
+        }
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
 }
